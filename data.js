601cfd5d544da323a039cc3765706ec1046b09ae
thomas ballard <thomasgeballard@gmail.com>
2023-05-22 16:16:01 +0100

    init commit

---
+++ data.js
@@ -0,0 +1,154 @@
+const skateSuggest = {
+  // Narrow Stunt Sakate options
+  AAA: {
+    name: "narrow-stunt-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  AAB: {
+    name: "narrow-stunt-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  AAC: {
+    name: "narrow-stunt-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Narrow Urban skate options
+  ABA: {
+    name: "narrow-urban-upto-250",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  ABB: {
+    name: "narrow-urban-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  ABC: {
+    name: "narrow-urban-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Narrow fitness skate options
+  ACA: {
+    name: "narrow-fitness-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  ACB: {
+    name: "narrow-fitness-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  ACC: {
+    name: "narrow-fitness-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Regular stunt skate options
+  BAA: {
+    name: "Regular-stunt-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  BAB: {
+    name: "Regular-stunt-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  BAC: {
+    name: "Regular-stunt-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Regular Urban skate options
+  BBA: {
+    name: "Regular-urban-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  BBB: {
+    name: "Regular-urban-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  BBC: {
+    name: "Regular-urban-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Regular fitness skate options
+  BCA: {
+    name: "Regular-fitness-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  BCB: {
+    name: "Regular-fitness-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  BCC: {
+    name: "Regular-fitness-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Wide stunt skate options
+  CAA: {
+    name: "wide-stunt-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  CAB: {
+    name: "wide-stunt-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  CAC: {
+    name: "wide-stunt-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Wide Urban skate options
+  CBA: {
+    name: "wide-urban-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  CBB: {
+    name: "wide-urban-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  CBC: {
+    name: "wide-urban-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+
+  // Wide fitness skate options
+  CCA: {
+    name: "wide-fitness-upto-200",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  CCB: {
+    name: "wide-fitness-upto-350",
+    info: "placeholder",
+    image: "placeholder"
+  },
+  CCC: {
+    name: "wide-fitness-350+",
+    info: "placeholder",
+    image: "placeholder"
+  },
+};
